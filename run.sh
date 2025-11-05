c4dab646036fa7636268b1ba48bd99a751ec47e4
zhalok <rahmanzhalok@gmail.com>
2025-11-05 23:06:53 +0600

    updated docker compose command

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker-compose run --rm python json_to_latext.py && docker-compose run --rm latex resume.tex
+docker compose run --rm python json_to_latext.py && docker compose run --rm latex resume.tex
